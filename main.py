7dad2c79bc163a1469b18690889854ebce7ae1ce
yerovy <45079497+yerovyespitia@users.noreply.github.com>
2025-03-31 19:01:15 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import yt_dlp
+
+url = 'https://www.youtube.com/watch?v='
+
+ydl_opts = {
+    'format': 'bestvideo[ext=mp4][vcodec^=avc1]+bestaudio[ext=m4a][acodec^=mp4a]/best[ext=mp4][vcodec^=avc1]',
+    'merge_output_format': 'mp4',
+    'outtmpl': '%(title)s.%(ext)s',
+    'restrictfilenames': True,
+}
+
+
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
